a25ce2c93a06deda53063025084bbec89d21a965
Alimşah Yıldırım <pumpings@hotmail.com>
2017-01-13 23:21:34 +0200

    Update objects.php

--- objects.php
+++ objects.php
@@ -21,10 +21,12 @@ $object->exampleInteger = 1;
 $object->exampleString = "Hello World";
 $object->exampleBoolean = true;
 $object->exampleFloat = 3.14;
+$object->exampleObject = new stdClass();
+$object->exampleObject->stringValue = "stdClass Object";
 
 var_dump($object->exampleInteger); # Output: int 1
 var_dump($object->exampleString); # Output: string 'Hello World' (length = 11)
 var_dump($object->exampleBoolean); # Output: boolean true
 var_dump($object->exampleFloat); # Output: Float: 3.14
-
+var_dump($object->exampleObject->stringValue); # Output: public 'stringValue' => string 'stdClass Object' (length=15)
 ?>
